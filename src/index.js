b18867f7723b01d83da9ab8be8a073496b5109bf
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-04-15 16:19:26 -0700

    update JS UI

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ $(document).ready(function() {
     const userAge = parseInt($('#user-age').val());
     const userExp = parseInt($('#user-exp').val());
     const newInput = new SpaceAge(userAge, userExp);
-    const mercAge = newInput.mercuryAge();
+    const mercAge = newInput.mercury;
     // const mercExp = newInput.mercuryExp();
-    const venusAge = newInput.venusAge();
+    const venusAge = newInput.venus;
     // const venusExp = newInput.venusExp();
-    const marsAge = newInput.marsAge();
-    const jupiterAge = newInput.jupiterAge();
+    const marsAge = newInput.mars;
+    const jupiterAge = newInput.jupiter;
     $('#mercury').append(`<p>Your age on <strong>Mercury</strong> is ${mercAge} years</p>`);
     $('#venus').append(`<p>Your age on <strong>Venus</strong> is ${venusAge} years</p>`);
     $('#mars').append(`<p>Your age on <strong>Mars</strong> is ${marsAge} years</p>`);
