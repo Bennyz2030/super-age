dd4add088e31205dc2d17be67adc5205da69f4b5
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-04-15 15:44:37 -0700

    change equations in BL and refactor tests

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -9,35 +9,35 @@ describe ('SpaceAge', ()=> {
   });
 
   test('should determine the users age on Mercury', ()=> {
-    expect(input.mercury).toEqual(9.6);
+    expect(input.mercury).toBeCloseTo(166.66, 1);
   }); 
 
   test('should determine the users age on Venus', ()=> {
-    expect(input.venus).toEqual(24.8);
+    expect(input.venus).toBeCloseTo(64.51, 1);
   });
 
   test('should determine the users age on Mars', ()=> {
-    expect(input.mars).toBeCloseTo(75.2);
+    expect(input.mars).toBeCloseTo(21.27, 1);
   });
 
   test('should determine the users age on Jupiter', ()=> {
-    expect(input.jupiter).toBeCloseTo(474.4);
+    expect(input.jupiter).toBeCloseTo(3.37, 1);
   });
 
   test('should determine how many years the user has left to live on Mercury', ()=> {
-    expect(input.mercuryExp()).toEqual(9.6);
+    expect(input.mercuryExp()).toBeCloseTo(166.66, 1);
   });
 
   test('should determine how many years the user has left to live on Venus', ()=> {
-    expect(input.venusExp()).toEqual(24.8);
+    expect(input.venusExp()).toBeCloseTo(64.51, 1);
   });
 
   test('should determine how many years the user has left to live on Mars', ()=> {
-    expect(input.marsExp()).toBeCloseTo(75.2);
+    expect(input.marsExp()).toBeCloseTo(21.28), 1;
   });
 
   test('should determine how many years the user has left to live on Jupiter', ()=> {
-    expect(input.jupiterExp()).toBeCloseTo(474.4);
+    expect(input.jupiterExp()).toBeCloseTo(3.37, 1);
   });
 });
 
@@ -50,18 +50,18 @@ describe ('SpaceAge', ()=> {
   });
 
   test('should return the amount of years the user has lived past thier life expectency on Mercury', ()=> {
-    expect(input.mercuryExp()).toEqual(9.6);
+    expect(input.mercuryExp()).toBeCloseTo(166.66, 1);
   });
 
   test('should return the amount of years the user has lived past thier life expectency on Venus', ()=> {
-    expect(input.venusExp()).toEqual(24.8);
+    expect(input.venusExp()).toBeCloseTo(64.51, 1);
   });
 
   test('should return the amount of years the user has lived past thier life expectency on Mars', ()=> {
-    expect(input.marsExp()).toBeCloseTo(75.2);
+    expect(input.marsExp()).toBeCloseTo(21.28), 1;
   });
 
   test('should return the amount of years the user has lived past thier life expectency on Jupiter', ()=> {
-    expect(input.jupiterExp()).toBeCloseTo(474.4);
+    expect(input.jupiterExp()).toBeCloseTo(3.37, 1);
   });
 });
--- src/js/galactic.js
+++ src/js/galactic.js
@@ -2,10 +2,10 @@ export default class SpaceAge {
   constructor(age, lifeExp) {
     this.age = age;
     this.lifeExp = lifeExp;
-    this.mercury = (this.age * 0.24);
-    this.venus = (this.age * 0.62);
-    this.mars = (this.age * 1.88);
-    this.jupiter = (this.age * 11.86);
+    this.mercury = (this.age / 0.24);
+    this.venus = (this.age / 0.62);
+    this.mars = (this.age / 1.88);
+    this.jupiter = (this.age / 11.86);
   }
 
   // mercuryAge() {
@@ -29,8 +29,7 @@ export default class SpaceAge {
   // }
 
   mercuryExp() {
-    this.mercury = this.age * 0.24;
-    this.lifeExp = this.lifeExp * 0.24;
+    this.lifeExp = this.lifeExp / 0.24;
     if (this.lifeExp < this.mercury) {
       return (this.lifeExp - this.mercury) * -1;
     } else {
@@ -39,8 +38,7 @@ export default class SpaceAge {
   }
 
   venusExp() {
-    this.venus = this.age * 0.62;
-    this.lifeExp = this.lifeExp * 0.62;
+    this.lifeExp = this.lifeExp / 0.62;
     if (this.lifeExp < this.venus) {
       return (this.lifeExp - this.venus) * -1;
     } else {
@@ -49,8 +47,7 @@ export default class SpaceAge {
   }
 
   marsExp() {
-    this.mars = this.age * 1.88;
-    this.lifeExp = this.lifeExp * 1.88;
+    this.lifeExp = this.lifeExp / 1.88;
     if (this.lifeExp < this.mars) {
       return (this.lifeExp - this.mars) * -1;
     } else {
@@ -59,8 +56,7 @@ export default class SpaceAge {
   }
 
   jupiterExp() {
-    this.jupiter = this.age * 11.86;
-    this.lifeExp = this.lifeExp * 11.86;
+    this.lifeExp = this.lifeExp / 11.86;
     if (this.lifeExp < this.jupiter) {
       return (this.lifeExp - this.jupiter) * -1;
     } else {
