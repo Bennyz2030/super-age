f04da740bb47f165461668e54853804d3976e526
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-04-15 09:34:32 -0700

    add beforeEach variable to galactic.tets.js

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -2,10 +2,19 @@ import { SpaceAge } from '../src/js/galactic.js';
 
 describe ('SpaceAge', ()=> {
 
+  let input;
+
+  beforeEach(()=> {
+    input = new SpaceAge(40)
+  });
+
   test('should determine the users age on Mercury', ()=> {
-    const input = new SpaceAge(40);
     input.mercuryAge();
     expect(input.mercury).toEqual(9.6);
   }); 
+
+  // test('should determine the users age on Venus', ()=> {
+
+  // })
 
 })
