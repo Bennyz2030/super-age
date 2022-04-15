4542d6ef95ada719b0903f77cd801007c3a97cc5
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-04-15 16:17:57 -0700

    delete changes to get full line coverage

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -8,26 +8,6 @@ export default class SpaceAge {
     this.jupiter = (this.age / 11.86);
   }
 
-  // mercuryAge() {
-  //   this.mercury = this.age * 0.24;
-  //   return this.mercury;
-  // }
-
-  // venusAge() {
-  //   this.venus = this.age * 0.62;
-  //   return this.venus;
-  // }
-
-  // marsAge() {
-  //   this.mars = this.age * 1.88;
-  //   return this.mars;
-  // }
-
-  // jupiterAge() {
-  //   this.jupiter = this.age * 11.86;
-  //   return this.jupiter;
-  // }
-
   mercuryExp() {
     this.lifeExp = this.lifeExp / 0.24;
     if (this.lifeExp < this.mercury) {
